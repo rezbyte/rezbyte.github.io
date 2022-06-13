9e549221276ff36433ec33f20cbbb75f1e3ab641
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-06-13 11:05:41 +0800

    Added static rendering

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
-	}
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false
+    }),
+    prerender: {
+      default: true
+    },
+    paths: {
+			base: '',
+		},
+  }
 };
 
 export default config;
